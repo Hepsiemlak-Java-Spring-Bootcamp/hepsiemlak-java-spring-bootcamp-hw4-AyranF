3008ed04428e6d9e6d8cbf5235c48b37f613b3de
Osman Atay Öztürk <97110647+AyranF@users.noreply.github.com>
2022-02-27 23:37:47 +0300

    Delete EmlakBurada-Email directory

--- EmlakBurada-Email/src/main/java/emlakburada/repository/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package emlakburada.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import emlakburada.model.Email;
-
-public interface EmailRepository extends JpaRepository<Email, Integer> {
-
-}
