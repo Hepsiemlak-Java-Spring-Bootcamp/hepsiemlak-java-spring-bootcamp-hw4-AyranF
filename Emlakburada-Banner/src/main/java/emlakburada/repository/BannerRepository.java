0be5959d52d46447d7570cd006d7cbd1c460e1fc
Osman Atay Öztürk <97110647+AyranF@users.noreply.github.com>
2022-02-28 01:10:04 +0300

    Delete Emlakburada-Banner directory

--- Emlakburada-Banner/src/main/java/emlakburada/repository/BannerRepository.java
+++
@@ -1,7 +0,0 @@
-package emlakburada.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import emlakburada.model.Banner;
-
-public interface BannerRepository extends JpaRepository<Banner, Integer> {}
