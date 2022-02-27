d4ed9dd21d511d8fbcce5c85df797aea1d0001eb
Atay <Atay@DESKTOP-PJT46FE>
2022-02-27 23:40:20 +0300

    Github deneme

---
+++ EmlakBurada-Jdbc/src/main/java/emlakburada/EmlakBuradaJdbcApplication.java
@@ -0,0 +1,13 @@
+package emlakburada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlakBuradaJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmlakBuradaJdbcApplication.class, args);
+	}
+
+}
